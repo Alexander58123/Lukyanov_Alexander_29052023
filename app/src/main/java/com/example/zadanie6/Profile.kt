e5e4903984ed265ec7f2909406ddf907220d5ada
Alexander58123 <alexandr.luckjanov@yandex.ru>
2023-07-08 13:46:45 +0300

    menu bottom correct

--- app/src/main/java/com/example/zadanie6/Profile.kt
+++ app/src/main/java/com/example/zadanie6/Profile.kt
@@ -2,10 +2,13 @@ package com.example.zadanie6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
     }
+
+
 }
